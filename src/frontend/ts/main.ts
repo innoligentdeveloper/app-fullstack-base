dc2c787c3e668219a888c6dfa4a33de219ad473f
Lender <innoligentdeveloper@gmail.com>
2021-05-17 22:06:23 -0300

    clase typescript1

--- src/frontend/ts/main.ts
+++ src/frontend/ts/main.ts
@@ -1,6 +1,24 @@
-
-function SayHello(){
-    let current_value = document.getElementById("textarea_1") as HTMLInputElement;
-    let new_value = "Hello world!!!" + "\n" + current_value.value;
-    document.getElementById("textarea_1").innerHTML = new_value;
+class Main{
+    public main():void {
+        console.log("SE EJECUTO EL MAIN");
+        let listaUsr:Array<User> = new Array<User>();
+        let usr1:User= new User(1, "Leandro", "leandro@ciribe.com.ar", true);
+        let usr2:User=new User(2, "Juan", "juan@ciribe.com.ar", false);
+        let usr3:User=new User(3, "Pablo", "pablo@ciribe.com.ar", true);
+        listaUsr.push(usr1);
+        listaUsr.push(usr2);
+        listaUsr.push(usr3);
+        for (let obj in listaUsr){
+            listaUsr[obj].printInfo();
+        }
+    }
+    public getElementById():HTMLElement{
+        return document.getElementById("boton"); //hace referencia al documento html
+    }
 }
+window.onload=function inicializar() {
+    let miObjMain:Main = new Main();
+    miObjMain.main();
+    let boton = miObjMain.getElementById();
+    boton.textContent="NUEVO TEXTO"
+};
